619570d6e48cf739202c903325619717a88379bc
Rick Carmack <riverguy64@gmail.com>
2017-07-27 15:57:37 -0500

    added classis.class

--- app/src/main/java/com/example/android/rcusapp/congregations.java
+++ app/src/main/java/com/example/android/rcusapp/congregations.java
@@ -1,7 +1,10 @@
 package com.example.android.rcusapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class congregations extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class congregations extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congregations);
         setTitle("RCUS > Congregations");
+
+        // Find the View that shows the classis category
+        TextView classis = (TextView) findViewById(R.id.byClassis);
+
+        // Set a click listener on that View
+        classis.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the classis category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ClassisActivity}
+                Intent classisIntent = new Intent(congregations.this, classis.class);
+
+                // Start the new activity
+                startActivity(classisIntent);
+            }
+        });
     }
 }
